903fbec93fae62523c6f6660be83cc4aad42ed09
Mansoor <adil.mansoor@capgemini.com>
2019-01-11 11:15:27 +0530

    app.js comment and structural changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*------Start of the application--------*/
 /*------Write 'npm install' to install all the dependencies-------------*/
-/*------Start the application by typing 'node app.js'-------------------*/
+/*------Start the application by the command 'node app.js'-------------------*/
 /*------You can edit the port number by editing the const variable portNumber------*/
 
 // we will use the require function to load the module
 const express = require('express') //express is the name of module which returns a function .
 const app = express(); // object created of type Express
-const portNumber = process.env.port || 8081; //if port has been specified in the environment it will pick up that
+const portNumber = process.env.port || 8081; //if port has been specified in the environment it will pick up that number or else the port defined
 const student = require('./routes/studentRoutes.js'); //getting the router object from studentRoutes
 const teacher = require('./routes/teacherRoutes.js');// getting the router object from teacherRoutes
 
@@ -17,15 +17,9 @@ app.put();
 app.delete();
 */
 
-
-//Basically we are telling that for the routes starting with the first argument url, use the second argument router
-app.use('/api/student', student);
-app.use('/api/teacher', teacher);
-
-
 /*
-*First app.get() has two argument. 
-*First argument is a url for the root of the website
+*First app.get() has two arguments. 
+*First argument is a url for the root of the website.
 *Second argument is a call back function which has two argument 'req' and 'res'.
 *So when the root of the url has been hit, the lambda callback function will be called and two paramters will be passed.
 */
@@ -34,7 +28,12 @@ app.get('/', (req, res) => {
 })
 
 
-//now for listening the endpoints on a given port
+//Basically we are telling that, for the routes starting with the first argument url, use the second argument router.
+app.use('/api/student', student);
+app.use('/api/teacher', teacher);
+
+
+//now for listening to the endpoints on a given port
 /*
 * First argument is the port on which you want the app to listen.
 * Second argument is an optional parameter.
